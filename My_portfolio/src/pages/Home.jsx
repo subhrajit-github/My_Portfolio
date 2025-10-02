30e97e1340e1b218771ceac6e106770bf5598d72
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-02 16:45:49 +0530

    commit

--- My_portfolio/src/pages/Home.jsx
+++ My_portfolio/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
 
     return (
