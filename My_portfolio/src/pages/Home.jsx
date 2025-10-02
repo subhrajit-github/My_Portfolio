e59e7eae59b9c0bf3aead2df650e7a5a7aeb480f
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-02 10:27:08 +0530

    commit

---
+++ My_portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+
+    return <div className="min-h-screen ">
+
+        {/* Theme Toggole */}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/*Footer */}
+    </div>;
+};
