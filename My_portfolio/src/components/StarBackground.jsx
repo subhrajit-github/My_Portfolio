2e0c94eb2fe59e3ce203e404551603adfc5f00bb
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-02 14:56:00 +0530

    commit

--- My_portfolio/src/components/StarBackground.jsx
+++ My_portfolio/src/components/StarBackground.jsx
@@ -10,6 +10,14 @@ export const StarBackground = () => {
     useEffect(()=>{
         generateStars();
         generateMeteors();
+const handleResize=()=>{
+            generateStars();
+            generateMeteors();
+        };
+        window.addEventListener("resize",handleResize);
+        return()=>{
+            window.removeEventListener("resize",handleResize);
+        };
 }, []);
 
     const generateStars = () => {
@@ -33,7 +41,7 @@ export const StarBackground = () => {
     };
 
     const generateMeteors = () => {
-        const numberOfMeteors=18;
+        const numberOfMeteors=14;
 
         const newMeteors=[];
         for(let i=0;i<numberOfMeteors;i++){
@@ -66,8 +74,8 @@ export const StarBackground = () => {
         {meteors.map((meteor) =>(
             <div key={meteor.id} className="meteor animate-meteor" 
             style={{
-                width:meteor.size+"px",
-                height:meteor.size+"px",
+                width:meteor.size*50+"px",
+                height:meteor.size*2+"px",
                 left:meteor.x+"%",
                 top:meteor.y+"%",
                 animationDelay:meteor.delay,
--- My_portfolio/src/pages/Home.jsx
+++ My_portfolio/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
             {/* Background Effects */}
             <StarBackground />
             {/* Navbar */}
-
+            
             {/* Main Content */}
 
             {/*Footer */}
