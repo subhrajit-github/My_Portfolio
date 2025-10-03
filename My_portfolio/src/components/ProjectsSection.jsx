a1abfd59115dda72180a53496336fd4ac0ca21ed
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-03 11:50:49 +0530

    commit

---
+++ My_portfolio/src/components/ProjectsSection.jsx
@@ -0,0 +1,34 @@
+
+
+const projects=[
+    {
+       id: 1,
+       title: "SaaS Landing Page",
+       description: "A Beautiful landing page using React and Tailwind.",
+       image: "/projects/project1.png",
+       tags: ["React","TailwindCSS","Supabase"],
+       demoUrl: "#",//live version url
+       githubUrl: "#"//github url
+    },
+    {
+        id: 2,
+        title: "Orbit Analytics Dashboard",
+        description:
+         "Interactive analytics dashboard with data visualization and filtering capabilities",
+        image: "/projects/project2.png",
+        tags: ["TypeScript","D3.js","Next.js"],
+        demoUrl: "#",//live version url
+        githubUrl: "#"//github url 
+    },
+    {
+        id: 3,
+        title: "Personal Portfolio Website",
+        description: "A responsive and modern portfolio website designed to showcase my skills,projects and experience.",
+        image: "" 
+    }
+];
+
+
+export const ProjectsSection = () =>{
+    return <section></section>;
+};
